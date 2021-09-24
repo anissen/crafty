3d8699c2829129e04e7ae99511f708745b4a5343
Anders Nissen <anders.nissen@trelleborg.com>
2021-09-24 13:09:28 +0200

    Use a local development version of Cosy for now

--- khafile.js
+++ khafile.js
@@ -13,7 +13,8 @@ project.addDefine('analyzer_optimize');
 
 project.addAssets('assets/**');
 project.addSources('src');
-project.addLibrary('cosy/src');
+// project.addLibrary('cosy/src');
+project.addLibrary('cosy-dev/src');
 // project.addLibrary('cosy'); // without /src requires a haxelib.json file with classPath set to "src"
 
 resolve(project);
