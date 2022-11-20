10e3c287b0b5f2ea4f3fb822136b885ed351239b
Anders Nissen <andnis@gmail.com>
2022-11-20 21:48:40 +0100

    Update haxe and kha

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node Kha/make --debug --compile --graphics metal --quiet && open ./build/osx-build/build/Debug/Crafty.app --args  ../../../../assets/cosy/follow.cosy
+node Kha/make --debug --compile --graphics metal --quiet && open ./build/osx-build/build/Debug/Crafty.app --args  ../../../../assets/cosy/breakout.cosy
