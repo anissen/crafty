a48777a27dff3e1f2202bf73230bc49da997b5dc
Anders Nissen <andnis@gmail.com>
2023-03-16 20:41:45 +0100

    Update

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-node Kha/make --debug --compile --graphics metal && open ./build/osx-build/build/Debug/Crafty.app --args  ../../../../assets/games/breakout/breakout-ecs.cosy
+# node Kha/make --debug --compile --graphics metal && open ./build/osx-build/build/Debug/Crafty.app --args  ../../../../assets/games/snake/snake.cosy 2>&1 output.txt
+node Kha/make --debug --compile --graphics metal && run.sh
+# node Kha/make --debug --compile --graphics metal && ./build/osx-build/build/Debug/Crafty.app/Contents/MacOS/Crafty assets/games/snake/snake.cosy
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+open ./build/osx-build/build/Debug/Crafty.app --args assets/games/snake/snake.cosy
+# ./build/osx-build/build/Debug/Crafty.app/Contents/MacOS/Crafty assets/games/snake/snake.cosy
